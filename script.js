f3003dd074aa46607f49102654f120cab0ddc934
Rafa541334 <dnrafa6@gmail.com>
2026-01-29 10:56:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+  document.addEventListener("mousemove", function(e) {
+    const cursor = document.querySelector('.custom-cursor');
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+  });
